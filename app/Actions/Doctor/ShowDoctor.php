10ac86f84d1905f700042c7aee6fa95317f7f4ff
Poojitha Penmetsa <poojithapenmetsa@my.unt.edu>
2021-11-15 18:55:55 -0600

    Fixed patient can book old times

--- app/Actions/Doctor/ShowDoctor.php
+++ app/Actions/Doctor/ShowDoctor.php
@@ -4,6 +4,7 @@ namespace App\Actions\Doctor;
 
 use App\Http\Resources\DoctorResource;
 use App\Models\Doctor;
+use Illuminate\Database\Query\Builder;
 use Lorisleiva\Actions\Concerns\AsController;
 
 class ShowDoctor
@@ -12,7 +13,13 @@ class ShowDoctor
 
     public function handle(Doctor $doctor)
     {
-        $doctor->load(['details', 'times', 'reviews']);
+        $doctor->load([
+            'details',
+            'times' => function ($q) {
+                return $q->where('date', '>=', now()->toDateString())->where('is_booked', false);
+            },
+            'reviews',
+        ]);
         $doctor->loadAvg('reviews', 'rate');
         return new DoctorResource($doctor);
     }
